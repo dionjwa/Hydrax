ea8b52da7796436991283359f5755928ffe2065e
Dion Amago <dionjw@gmail.com>
2010-11-11 10:13:36 -0600

    Cleanup

--- demo/basicEngineDemo01/src/Demo.hx
+++ demo/basicEngineDemo01/src/Demo.hx
@@ -12,6 +12,7 @@ import com.pblabs.engine.core.PBContext;
 import com.pblabs.engine.core.PBGame;
 import com.pblabs.engine.core.PropertyReference;
 import com.pblabs.engine.input.InputKey;
+
 using com.pblabs.components.scene.SceneComponentUtil;
 using com.pblabs.components.tasks.TaskUtil;
 using com.pblabs.engine.util.PBUtil;
@@ -25,26 +26,15 @@ class Demo extends flash.display.Sprite
         this.name = "Demo";
         var game = new PBGame();
         
-        
         var context = game.allocate(PBContext);
         game.pushContext(context);
         var scene2D = context.addSingletonComponent(Scene2DManager);
-        context.injector.mapValue(Scene2DManager, new PropertyReference("#Scene2DManager.Scene2DManager"), "Scene2DManager");
         var layer = scene2D.addLayer(SceneLayer, "defaultLayer");
-        #if debug
-        com.pblabs.util.Assert.isTrue(scene2D.owner.deferring == false);
-        #end
-        
         
         var so = context.createBaseSceneEntity("SomeSceneObj");
-        // so.deferring = true;
-        // so.addComponent(context.allocate(LocationComponent));
-        // so.addComponent(context.allocate(AngleComponent));
         var blob  = context.allocate(BlobDisplayComponent);
         blob.parentProperty = layer.entityProp();
         so.addComponent(blob);
-        
-        // so.addComponent(context.allocate(TaskComponent));
         so.deferring = false;
         
         so.addTask(LocationTask.CreateEaseOut(100, 100, 3));
@@ -54,7 +44,6 @@ class Demo extends flash.display.Sprite
     {
         flash.Lib.current.addChild(new Demo());
     }
-
 }
 
 
