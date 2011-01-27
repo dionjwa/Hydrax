d8b0a7a1363890a7132f775c5038a9c85a36c7ab
Dion Amago <dionjw@gmail.com>
2011-01-27 14:37:22 -0600

    Additional util methods.

--- src/com/pblabs/engine/core/SignalBondManager.hx
+++ src/com/pblabs/engine/core/SignalBondManager.hx
@@ -15,16 +15,16 @@ import hsl.haxe.Bond;
 import hsl.haxe.Signaler;
 
 using com.pblabs.engine.util.PBUtil;
+using com.pblabs.util.StringUtil;
 
 /**
-  * Manages signal bonds for convenient access to destroy.
-  * Every object that uses signals must make sure to remove 
-  * all listeners when destroyed.  Rather than having multiple 
-  * objects implement the same functionality, this class allows
-  * an object to register and remove all listeners.
-  *
-  * The suggested use is to register listeners on initialization,
-  * and then call destroyBonds on destruction.
+  * Holds signal bindings for IEntityComponents.  When the Entity is 
+  * reset or destroyed, all signals bound via this class are also destroyed.
+  * That means:
+  *   -you must intialized the IEntity before adding signals so that the 
+  *	 components have unique keys (using PropertyReferences)
+  *   -bind the signals in the EntityComponent.onReset method.
+  *   -use SignalBondManager.bindSignal to bind the signals.
   */
 class SignalBondManager extends ArrayMultiMap<String, Bond>,
 	implements IPBManager, implements haxe.rtti.Infos
@@ -35,14 +35,31 @@ class SignalBondManager extends ArrayMultiMap<String, Bond>,
 	public static function bindSignal <T>(component :IEntityComponent, signaler :Signaler<T>, listener :T->Dynamic) :Void
 	{
 		com.pblabs.util.Assert.isNotNull(component, "component is null");
-		com.pblabs.util.Assert.isNotNull(signaler, "signaler is null");
-		com.pblabs.util.Assert.isNotNull(listener, "listener is null");
 		com.pblabs.util.Assert.isNotNull(component.context, "component.context is null");
 		
 		var bonds = component.context.getManager(SignalBondManager);
 		com.pblabs.util.Assert.isNotNull(bonds, "SignalBondManager is null");
 		
 		bonds.bind(component, signaler, listener);
+	}
+	
+	public static function destroyAllBonds (c :IEntityComponent) :Void
+	{
+		com.pblabs.util.Assert.isNotNull(c, "component is null");
+		com.pblabs.util.Assert.isNotNull(c.context, "component.context is null");
+		
+		var bonds = c.context.getManager(SignalBondManager);
+		com.pblabs.util.Assert.isNotNull(bonds, "SignalBondManager is null");
+		bonds.destroyBonds(c.entityPropString());
+	}
+	
+	/**
+	  * Assumes that signals are rebound on the IEntityComponentonReset() call.
+	  */
+	public static function resetBonds (c :IEntityComponent) :Void
+	{
+		destroyAllBonds(c);
+		c.reset();
 	}
 	
 	public function new ()
@@ -56,6 +73,8 @@ class SignalBondManager extends ArrayMultiMap<String, Bond>,
 		com.pblabs.util.Assert.isNotNull(signaler, "signaler is null");
 		com.pblabs.util.Assert.isNotNull(listener, "listener is null");
 		com.pblabs.util.Assert.isTrue(component.isRegistered, "component is unregistered");
+		com.pblabs.util.Assert.isTrue(component.owner.isLiveObject, "component entity is not initialized.  Entities much be initialized first");
+		com.pblabs.util.Assert.isFalse(component.owner.name.isBlank(), "owner has no name");
 		
 		var key = component.entityPropString();
 		var bond = signaler.bind(listener);
