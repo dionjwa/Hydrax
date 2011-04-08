cb7e8a21baf846f5ab6d27a79949be467b907758
Dion Amago <dionjw@gmail.com>
2011-04-08 08:29:49 -0500

    Add license headers.

--- src/com/pblabs/util/ds/sets/ArraySet.hx
+++ src/com/pblabs/util/ds/sets/ArraySet.hx
@@ -1,5 +1,12 @@
+/*******************************************************************************
+ * Hydrax: haXe port of the PushButton Engine
+ * Copyright (C) 2010 Dion Amago
+ * For more information see http://github.com/dionjwa/Hydrax
+ *
+ * This file is licensed under the terms of the MIT license, which is included
+ * in the License.html file at the root directory of this SDK.
+ ******************************************************************************/
 package com.pblabs.util.ds.sets;
-
 import com.pblabs.util.ds.Set;
 using com.pblabs.util.ArrayUtil;
 
