26183956c0cd0e2db7c638d1cc19d65c07bcd5eb
dionjwa <dionjw@gmail.com>
2011-07-25 12:55:18 -0500

    Recheck bounds when text is modified.

--- src/com/pblabs/components/scene2D/flash/TextComponent.hx
+++ src/com/pblabs/components/scene2D/flash/TextComponent.hx
@@ -7,23 +7,60 @@
  * in the License.html file at the root directory of this SDK.
  ******************************************************************************/
 package com.pblabs.components.scene2D.flash;
+
+import flash.display.Sprite;
+
 import flash.text.TextField;
+import flash.text.TextFieldAutoSize;
 
 class TextComponent extends SceneComponent
 {
 	public var text (get_text, set_text) :String;
+	public var textField (get_textField, set_textField) :TextField;
+	function get_textField () :TextField
+	{
+		return _text;
+	}
+	
+	function set_textField (val :TextField) :TextField
+	{
+		_text = val;
+		return val;
+	}
 	var _text :TextField;
+	var _sprite :Sprite;
 	
 	public function new ()
 	{
 		super();
+		_sprite = com.pblabs.util.SpriteUtil.create();
 		_text = new TextField();
-		_text.width = 800;
-		_text.height = 800;
+		_text.width = 200;
+		_text.height = 30;
+		
 		_text.mouseEnabled = false;
-		_text.wordWrap = true;
+		_text.wordWrap = false;
+		_text.multiline = true;
 		_text.mouseWheelEnabled = false;
-		_displayObject = _text;
+		_sprite.addChild(_text);
+		_displayObject = _sprite;
+		registrationPoint.x = 0;
+		registrationPoint.y = 0;
+		text = " ";
+	}
+	
+	override function recomputeBounds () :Void
+	{
+		super.recomputeBounds();
+		
+		#if flash
+		var localDimensions = _text.getBounds(_sprite);
+		#elseif cpp
+		var localDimensions = _text.nmeGetPixelBounds(_sprite);
+		#end
+		
+		_text.x = (-localDimensions.width / 2);
+		_text.y = (-localDimensions.height / 2);
 	}
 
 	function get_text () :String
@@ -34,6 +71,7 @@ class TextComponent extends SceneComponent
 	function set_text (val :String) :String
 	{
 		_text.text = val;
+		recomputeBounds();
 		return val;
 	}
 }
