d48c7057175e5e35ac4129447f6ec57b6915bda2
Dion Amago <dionjw@gmail.com>
2011-09-01 11:21:10 -0500

    Make compile safe for non-js platforms.

--- src/com/pblabs/components/input/TouchInputManager.hx
+++ src/com/pblabs/components/input/TouchInputManager.hx
@@ -1,12 +1,15 @@
 /*******************************************************************************
- * Hydrax: haXe port of the PushButton Engine
+ * Hydrax :haXe port of the PushButton Engine
  * Copyright (C) 2010 Dion Amago
- * For more information see http://github.com/dionjwa/Hydrax
+ * For more information see http ://github.com/dionjwa/Hydrax
  *
  * This file is licensed under the terms of the MIT license, which is included
  * in the License.html file at the root directory of this SDK.
  ******************************************************************************/
 package com.pblabs.components.input;
+
+#if js
+
 import com.pblabs.geom.Vector2;
 
 import hsl.haxe.Signaler;
@@ -19,146 +22,148 @@ using hsl.js.data.Touch.TouchListIterator;
  * Javascript only ATM.  What other platforms support gestures?
  * 
  * Funnels gesture events into signals.
- * Can funnel into MouseInputManager if no specific gesture logic.
+ * Can funnel into MouseInputManager if no specific gesture Logic.
  * 
  */
 @:require(js)
 class TouchInputManager extends BaseInputManager
 {
-    @inject
-    var mouse :MouseInputManager;
-    
-    public var touchStart (default, null) :Signaler<TouchEvent>;
-    public var touchMove (default, null) :Signaler<TouchEvent>;
-    public var touchEnd (default, null) :Signaler<TouchEvent>;
-    
-    /** Currently not implemented */
-    public var convertTouchEventsToMouse :Bool;
-    
-    public function new (?convertToMouseEvents :Bool = true)
-    {
-        super();
-        convertTouchEventsToMouse = convertToMouseEvents;
-    }
-    
-    override public function startup () :Void
-    {
-        super.startup();
-        bindSignals();
-        
-        /**
-          * Channel touch events into mouse events so that we 
-          * can just handle mouse events.
-          */
-        if (convertTouchEventsToMouse && mouse != null) {
-            var self = this;
-            touchStart.bind(function (e :TouchEvent) :Void {
-                e.preventDefault();
-                #if testing
-                var s = "";
-                for (touch in e.touches) {
-                    s += " (" + touch.clientX + ", " + touch.clientY +")";
-                }
-                js.Lib.document.getElementById("haxe:touchStart").innerHTML = "touchStart: " + s;
-                #end
-                
-                //Compute the average touch if the user puts down more than one finger at the same time
-                var meanTouch = new Vector2();
-                for (touch in e.changedTouches) {
-                    meanTouch.x += touch.clientX;
-                    meanTouch.y += touch.clientY;
-                }
-                meanTouch.x /= e.changedTouches.length;
-                meanTouch.y /= e.changedTouches.length;
-                
-                //When converting to mouse input, ignore extra touches
-                if (e.touches.length > 1 && e.changedTouches.length >= 1) {
-                    return;
-                }
-                var touch = e.touches.item(0);
-                self._firstTouchId = touch.identifier;
-                
-                self.mouse.manualMouseDown(meanTouch.x, meanTouch.y, e.touches.length);
-                
-                // for (touch in e.touches) {
-                //     //Just treat the first touch as the mouse
-                //     mouseInput.manualMouseDown(touch.clientX, touch.clientY);
-                //     break;
-                // }
-            });
-            
-            touchMove.bind(function (e :TouchEvent) :Void {
-                e.preventDefault();
-                #if testing
-                var s = "";
-                for (touch in e.changedTouches) {
-                    s += " (" + touch.clientX + ", " + touch.clientY +")";
-                }
-                js.Lib.document.getElementById("haxe:touchMove").innerHTML = "touchMove: " + s;
-                #end
-                
-                // var touch = e.touches.item(0);
-                // mouseInput.manualMouseMove(touch.clientX, touch.clientY);
-                
-                for (touch in e.changedTouches) {
-                    if (touch.identifier == self._firstTouchId) {
-                        self.mouse.manualMouseMove(touch.clientX, touch.clientY, e.touches.length);
-                        break;
-                    }
-                }
-            });
-            
-            touchEnd.bind(function (e :TouchEvent) :Void {
-                e.preventDefault();
-                #if testing
-                var s = "";
-                for (touch in e.changedTouches) {
-                    s += " (" + touch.clientX + ", " + touch.clientY +")";
-                }
-                js.Lib.document.getElementById("haxe:touchEnd").innerHTML = "touchEnd: " + s;
-                #end
-                
-                for (touch in e.changedTouches) {
-                    if (touch.identifier == self._firstTouchId) {
-                        self.mouse.manualMouseUp(touch.clientX, touch.clientY, e.touches.length);
-                        break;
-                    }
-                }
-            });
-        }
-        
-        if (convertTouchEventsToMouse && mouse == null) {
-            com.pblabs.util.Log.error("Convert touch events to mouse, but no MouseInputManager");
-        }
-        
-    }
-    
-    override public function shutdown () :Void
-    {
-        super.shutdown();
-        
-        #if js
-        #if debug
-        com.pblabs.util.Assert.isFalse(touchStart.isListenedTo);
-        com.pblabs.util.Assert.isFalse(touchMove.isListenedTo);
-        com.pblabs.util.Assert.isFalse(touchEnd.isListenedTo);
-        #end
-        touchStart = null;
-        touchMove = null;
-        touchEnd = null;
-        #end
-    }
-    
-    function bindSignals () :Void
-    {
-        #if js
-        touchStart = new hsl.js.translating.JSSignaler(this, js.Lib.document, JSEventType.TOUCHSTART, new hsl.js.translation.touch.TouchTranslator());
-        touchMove = new hsl.js.translating.JSSignaler(this, js.Lib.document, JSEventType.TOUCHMOVE, new hsl.js.translation.touch.TouchTranslator());
-        touchEnd = new hsl.js.translating.JSSignaler(this, js.Lib.document, JSEventType.TOUCHEND, new hsl.js.translation.touch.TouchTranslator());
-        #else
-        com.pblabs.util.Log.error("Platform gestures are not yet implemented.  Currently JS only.");
-        #end
-    }
-    
-    var _firstTouchId :Int;
+	@inject
+	var mouse :MouseInputManager;
+	
+	public var touchStart (default, null) :Signaler<TouchEvent>;
+	public var touchMove (default, null) :Signaler<TouchEvent>;
+	public var touchEnd (default, null) :Signaler<TouchEvent>;
+	
+	/** Currently not implemented */
+	public var convertTouchEventsToMouse :Bool;
+	
+	public function new (?convertToMouseEvents :Bool = true)
+	{
+		super();
+		convertTouchEventsToMouse = convertToMouseEvents;
+	}
+	
+	override public function startup () :Void
+	{
+		super.startup();
+		bindSignals();
+		
+		/**
+		  * Channel touch events into mouse events so that we 
+		  * can just handle mouse events.
+		  */
+		if (convertTouchEventsToMouse && mouse != null) {
+			var self = this;
+			touchStart.bind(function (e :TouchEvent) :Void {
+				e.preventDefault();
+				#if testing
+				var s = "";
+				for (touch in e.touches) {
+					s += " (" + touch.clientX + ", " + touch.clientY +")";
+				}
+				js.Lib.document.getElementById("haxe :touchStart").innerHTML = "touchStart :" + s;
+				#end
+				
+				//Compute the average touch if the user puts down more than one finger at the same time
+				var meanTouch = new Vector2();
+				for (touch in e.changedTouches) {
+					meanTouch.x += touch.clientX;
+					meanTouch.y += touch.clientY;
+				}
+				meanTouch.x /= e.changedTouches.length;
+				meanTouch.y /= e.changedTouches.length;
+				
+				//When converting to mouse input, ignore extra touches
+				if (e.touches.length > 1 && e.changedTouches.length >= 1) {
+					return;
+				}
+				var touch = e.touches.item(0);
+				self._firstTouchId = touch.identifier;
+				
+				self.mouse.manualMouseDown(meanTouch.x, meanTouch.y, e.touches.length);
+				
+				// for (touch in e.touches) {
+				//	 //Just treat the first touch as the mouse
+				//	 mouseInput.manualMouseDown(touch.clientX, touch.clientY);
+				//	 break;
+				// }
+			});
+			
+			touchMove.bind(function (e :TouchEvent) :Void {
+				e.preventDefault();
+				#if testing
+				var s = "";
+				for (touch in e.changedTouches) {
+					s += " (" + touch.clientX + ", " + touch.clientY +")";
+				}
+				js.Lib.document.getElementById("haxe :touchMove").innerHTML = "touchMove :" + s;
+				#end
+				
+				// var touch = e.touches.item(0);
+				// mouseInput.manualMouseMove(touch.clientX, touch.clientY);
+				
+				for (touch in e.changedTouches) {
+					if (touch.identifier == self._firstTouchId) {
+						self.mouse.manualMouseMove(touch.clientX, touch.clientY, e.touches.length);
+						break;
+					}
+				}
+			});
+			
+			touchEnd.bind(function (e :TouchEvent) :Void {
+				e.preventDefault();
+				#if testing
+				var s = "";
+				for (touch in e.changedTouches) {
+					s += " (" + touch.clientX + ", " + touch.clientY +")";
+				}
+				js.Lib.document.getElementById("haxe :touchEnd").innerHTML = "touchEnd :" + s;
+				#end
+				
+				for (touch in e.changedTouches) {
+					if (touch.identifier == self._firstTouchId) {
+						self.mouse.manualMouseUp(touch.clientX, touch.clientY, e.touches.length);
+						break;
+					}
+				}
+			});
+		}
+		
+		if (convertTouchEventsToMouse && mouse == null) {
+			com.pblabs.util.Log.error("Convert touch events to mouse, but no MouseInputManager");
+		}
+		
+	}
+	
+	override public function shutdown () :Void
+	{
+		super.shutdown();
+		
+		#if js
+		#if debug
+		com.pblabs.util.Assert.isFalse(touchStart.isListenedTo);
+		com.pblabs.util.Assert.isFalse(touchMove.isListenedTo);
+		com.pblabs.util.Assert.isFalse(touchEnd.isListenedTo);
+		#end
+		touchStart = null;
+		touchMove = null;
+		touchEnd = null;
+		#end
+	}
+	
+	function bindSignals () :Void
+	{
+		#if js
+		touchStart = new hsl.js.translating.JSSignaler(this, js.Lib.document, JSEventType.TOUCHSTART, new hsl.js.translation.touch.TouchTranslator());
+		touchMove = new hsl.js.translating.JSSignaler(this, js.Lib.document, JSEventType.TOUCHMOVE, new hsl.js.translation.touch.TouchTranslator());
+		touchEnd = new hsl.js.translating.JSSignaler(this, js.Lib.document, JSEventType.TOUCHEND, new hsl.js.translation.touch.TouchTranslator());
+		#else
+		com.pblabs.util.Log.error("Platform gestures are not yet implemented.  Currently JS only.");
+		#end
+	}
+	
+	var _firstTouchId :Int;
 }
+
+#end
