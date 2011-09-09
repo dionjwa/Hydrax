bc5fadaf57eee19bf7bc89537e9f9ec79c19fdee
dionjwa <dionjw@gmail.com>
2011-09-09 16:35:59 -0500

    Test javascript too.

--- bin/testCompileAll.py
+++ bin/testCompileAll.py
@@ -3,8 +3,15 @@
 #Create the rsrc/audio.swf containing all mp3s in the rsrc folder
 import os, os.path, string, sys, shutil
 
-os.system("bin/createImportAllForSwc.py src src/Test.hx")
-os.system("haxe -main Test -cp src -swf build/delete.swf -lib polygonal -lib feffects -lib IterTools -lib ArrayTools -lib ListTools -lib hsl-1 ")
-os.remove("build/delete.swf")
-os.remove("src/Test.hx")
+print "Testing Flash"
+os.system("bin/createImportAllForSwc.py src src/TestHydraxCompilation.hx")
+os.system("haxe -main TestHydraxCompilation -cp src -swf build/delete.swf -lib polygonal -lib feffects -lib hsl-1 -lib gm2d")
 
+print "Testing javascript"
+os.system("bin/createImportAllForJs.py src src/TestHydraxCompilation.hx")
+os.system("haxe -main TestHydraxCompilation -cp src -js build/test.js -cp ../libdamago/lib/bdog-redis -cp ../libdamago/lib/bdog-core --remap flash:jeash -lib polygonal -lib feffects  -lib hsl-1 -lib html5 -lib jQueryExtern -lib gm2d -lib nodejs -lib nodejs_externs")
+
+filesToDelete = ["build/delete.swf", "src/TestHydraxCompilation.hx" "build/test.js"]
+for f in filesToDelete:
+	if os.path.exists(f):
+		os.remove(f)
