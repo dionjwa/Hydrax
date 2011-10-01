55f305e654d5af71492e0faa67b5bf43d2b27c63
dionjwa <dionjw@gmail.com>
2011-10-01 01:24:13 -0500

    Screen navigation demo fixes/improvements.

--- demo/screennavigation/src/BoxPanelImage.hx
+++ demo/screennavigation/src/BoxPanelImage.hx
@@ -1,13 +1,220 @@
 package ;
 
+import com.pblabs.components.minimalcomp.Component;
+import com.pblabs.components.minimalcomp.Container;
+import com.pblabs.components.scene2D.BitmapRenderer;
+import com.pblabs.components.scene2D.GraphicsComponent;
+import com.pblabs.components.spatial.SpatialComponent;
+import com.pblabs.engine.time.IProcessManager;
+import com.pblabs.geom.Vector2;
+import com.pblabs.util.F;
+using com.pblabs.engine.core.SignalBondManager;
+using com.pblabs.util.StringUtil;
+
 /**
   * An image that redraws itself if the container component is redrawn.
   */
 class BoxPanelImage
+	#if flash
+	extends GraphicsComponent
+	#elseif js
+	extends BitmapRenderer
+	#end
 {
+	public var color (get_color, set_color) :Int;
+	var _color :Int;
+	function get_color () :Int
+	{
+		return _color;
+	}
+	
+	function set_color (val :Int) :Int
+	{
+		_color = val;
+		redraw();
+		return val;
+	}
+	
+	public var lineWidth (get_lineWidth, set_lineWidth) :Float;
+	var _lineWidth :Float;
+	function get_lineWidth () :Float
+	{
+		return _lineWidth;
+	}
+	
+	function set_lineWidth (val :Float) :Float
+	{
+		_lineWidth = val;
+		redraw();
+		return val;
+	}
+	
+	public var dividerColor (get_dividerColor, set_dividerColor) :Int;
+	var _dividerColor :Int;
+	function get_dividerColor () :Int
+	{
+		return _dividerColor;
+	}
+	function set_dividerColor (val :Int) :Int
+	{
+		_dividerColor = val;
+		redraw();
+		return val;
+	}
+	
+	public var dividerLineWidth (get_dividerLineWidth, set_dividerLineWidth) :Float;
+	var _dividerLineWidth :Float;
+	function get_dividerLineWidth () :Float
+	{
+		return _dividerLineWidth;
+	}
+	function set_dividerLineWidth (val :Float) :Float
+	{
+		_dividerLineWidth = val;
+		redraw();
+		return val;
+	}
+	
+	public var roundCorner (get_roundCorner, set_roundCorner) :Int;
+	var _roundCorner :Int;
+	function get_roundCorner () :Int
+	{
+		return _roundCorner;
+	}
+	function set_roundCorner (val :Int) :Int
+	{
+		_roundCorner = val;
+		redraw();
+		return val;
+	}
+	
+	
+	
 	public function new ()
 	{
 		super();
+		setDefaults();
+	}
+	
+	override function onReset () :Void
+	{
+		super.onReset();
+		var component = owner.getComponent(Container);
+		if (component != null) {
+			bindSignal(component.redrawSignal, F.ignoreArg(redraw));
+		}
+	}
+	
+	override function onRemove () :Void
+	{
+		super.onRemove();
+		setDefaults();
+	}
+	
+	override function setDefaults () :Void
+	{
+		super.setDefaults();
+		_color = _dividerColor = 0x000000;
+		_lineWidth = _dividerLineWidth = 1;
+		_roundCorner = 10;
+	}
+	
+	public function redraw () :Void
+	{
+		context.getManager(IProcessManager).callLater(redrawInternal);
+	}
+	
+	function redrawInternal () :Void
+	{
+		var component = owner.getComponent(Container);
+		com.pblabs.util.Assert.isNotNull(component, ' component is null');
+		
+		var bounds = component.bounds;
+		
+		if (Math.isNaN(bounds.centerX) || Math.isNaN(bounds.centerY)) {
+			return;
+		}
+		
+		var gap = 3;
+		var round = _roundCorner;
+		
+		#if flash
+			var g = graphics;
+			g.clear();
+			
+			//Draw the box
+			
+			g.lineStyle(_lineWidth, _color);
+			g.drawRoundRect(-_lineWidth - gap, -_lineWidth - gap, component.width + _lineWidth * 2 + gap * 2, component.height + _lineWidth * 2 + gap * 2, round);
+			registrationPoint = new Vector2(x - bounds.xmin, y - bounds.ymin);
+			
+			#if js
+			g.jeashRender();
+			#end
+			
+			//Draw the dividers
+			var curY = 0.0;
+			for (ii in 0...component.children.length) {
+				var c = component.children[ii];
+				if (ii == 0) {
+					curY += c.height;
+					continue;
+				}
+				g.moveTo(0, curY);
+				g.lineTo(bounds.intervalX, curY);
+				curY += c.height;
+			}
+			
+			#if js
+			g.jeashRender();
+			#end
+		#elseif js
+		var canvas :Canvas = cast js.Lib.document.createElement("canvas");
+		canvas.width = Std.int(bounds.intervalX + gap * 2 + _lineWidth * 2);
+		canvas.height = Std.int(bounds.intervalY + gap * 2 + _lineWidth * 2);
+		
+		var ctx = canvas.getContext('2d');
+		
+		var w = Std.int(bounds.intervalX);
+		var h = Std.int(bounds.intervalY);
+		
+		ctx.beginPath();
+		ctx.moveTo(round, 0);
+		ctx.lineTo(w-round, 0);
+		ctx.quadraticCurveTo(w, 0, w, round);
+		ctx.lineTo(w, h-round);
+		ctx.quadraticCurveTo(w, h, w-round, h);
+		ctx.lineTo(round, h);
+		ctx.quadraticCurveTo(0, h, 0, h-round);
+		ctx.lineTo(0, round);
+		ctx.quadraticCurveTo(0, 0, round, 0);
+		
+		ctx.lineWidth = _lineWidth;
+		ctx.strokeStyle = _color.toColorString("#");
+		ctx.stroke();
+		
+		ctx.beginPath();
+		//Draw the dividers
+		var curY = 0.0;
+		for (ii in 0...component.children.length) {
+			var c = component.children[ii];
+			if (ii == 0) {
+				curY += c.height;
+				continue;
+			}
+			ctx.moveTo(gap, curY);
+			ctx.lineTo(bounds.intervalX - gap, curY);
+			curY += c.height;
+		}
+		ctx.lineWidth = _dividerLineWidth;
+		ctx.strokeStyle = _dividerColor.toColorString("#");
+		ctx.stroke();
+		
+		bitmapData = canvas;
+		registrationPoint = new Vector2(x - bounds.xmin, y - bounds.ymin);
+		#end
+		
+		
 	}
 
 }
--- demo/screennavigation/src/Demo.hx
+++ demo/screennavigation/src/Demo.hx
@@ -3,21 +3,14 @@ package ;
 import com.pblabs.components.input.InputManager;
 import com.pblabs.components.input.MouseInputManager;
 import com.pblabs.components.minimalcomp.Container;
-import com.pblabs.components.minimalcomp.HBox;
-import com.pblabs.components.scene2D.BaseSceneComponent;
-import com.pblabs.components.scene2D.BaseSceneLayer;
+import com.pblabs.components.scene2D.CircleShape;
 import com.pblabs.components.scene2D.SceneAlignment;
-import com.pblabs.components.scene2D.SceneUtil;
 import com.pblabs.components.ui.ScreenTransitions;
 import com.pblabs.engine.core.IEntity;
-import com.pblabs.engine.core.ObjectType;
-import com.pblabs.engine.core.PBContext;
 import com.pblabs.engine.core.PBGame;
 import com.pblabs.engine.core.PBGameBase;
 import com.pblabs.engine.resource.BitmapCacheResource;
 import com.pblabs.engine.resource.IResourceManager;
-import com.pblabs.engine.resource.ResourceToken;
-import com.pblabs.engine.resource.ResourceType;
 import com.pblabs.engine.resource.Source;
 import com.pblabs.engine.resource.SvgResources;
 import com.pblabs.util.svg.SvgReplace;
@@ -32,6 +25,7 @@ using com.pblabs.components.scene2D.HierarchyManager;
 using com.pblabs.components.scene2D.ImageTools;
 using com.pblabs.components.scene2D.SceneUtil;
 using com.pblabs.components.tasks.TaskUtil;
+using com.pblabs.components.ui.InputButtonOverlay;
 using com.pblabs.engine.util.PBUtil;
 using com.pblabs.util.StringUtil;
 
@@ -39,30 +33,25 @@ class Demo
 {
 	public function new() 
 	{
+		#if js
+		com.pblabs.util.JsDebugUtil.traceToConsole(false);
+		#end
 		
-		var width = untyped __js__("screen.width");
-		// trace('width=' + width);
 		com.pblabs.engine.debug.Log.setup();
 		game = new PBGame();
 		game.registerManager(MouseInputManager, new MouseInputManager());
 		
 		#if js
-		// untyped {
-		// 	jo.load();
-		// }
-		com.pblabs.util.JsDebugUtil.traceToConsole();
 		game.registerManager(com.pblabs.components.input.TouchInputManager, new com.pblabs.components.input.TouchInputManager());
 		game.registerManager(com.pblabs.components.input.GestureInputManager, new com.pblabs.components.input.GestureInputManager());
-		game.registerManager(com.pblabs.components.input.OrientationManager, new com.pblabs.components.input.OrientationManager());
+		
 		#end
+		game.registerManager(com.pblabs.components.input.OrientationManager, new com.pblabs.components.input.OrientationManager());
 		game.registerManager(InputManager, new InputManager());
 		var input = game.getManager(InputManager);
 		
 		
 		var rsrc = game.getManager(IResourceManager);
-		#if flash
-		// rsrc.addResource(new com.pblabs.engine.resource.flash.SwfResource("resources", Source.url("../rsrc/resources.swf")));
-		#end
 		rsrc.addResource(game.allocate(SvgResources));
 		rsrc.addResource(game.allocate(BitmapCacheResource));
 		Resources.init();
@@ -103,33 +92,23 @@ class DemoPagedScreen extends com.pblabs.components.ui.PagedScreen
 	{
 		super.setup();
 		
+		addSingletonComponent(com.pblabs.components.ui.InputButtonOverlay);
+		
 		//Setup the context buttons
 		var topCenter = _layerForeground.scene.getAlignedPoint(SceneAlignment.TOP_CENTER);
 		topCenter.y += 200;
-		var vboxButtons = createVBox(_layerForeground)
-			.setLocation(topCenter.x, topCenter.y);
-			// .setSceneAlignment(SceneAlignment.TOP_CENTER, _layerBackground);
+		_contextVbox = createVBox(_layerForeground)
+			.addSceneComponent(BoxPanelImage, _layerForeground)
 			
+			.setLocation(topCenter.x, topCenter.y);
+		_contextVbox.getComponent(BoxPanelImage).color = 0x000000;
+		_contextVbox.getComponent(BoxPanelImage).dividerLineWidth = 0.5;
 			
 		var game = getManager(PBGameBase);
 		var jobs :Array<Dynamic> = [
-			// "Pop context", callback(game.popContext, ScreenTransitions.scrollBack), 
 			"Push context", callback(game.pushContext, DemoPagedScreen, ScreenTransitions.scrollForward),
-			// "Push context", callback(game.pushContext, DemoPagedScreen, ScreenTransitions.scrollForward),
+			"Pop context", callback(game.popContext, ScreenTransitions.scrollBack),
 		];
-		
-		
-		
-		// untyped __js__('var button = new joButton("Awesome!")');
-		// untyped __js__('var main = new joCard([ button ])');
-		// untyped {
-			// .selectEvent.subscribe(function() {
-			// 	trace("You clicked a button");
-			// });
-			
-			// dump the controls into a card
-			// var main = new joCard([ button ]);
-    	// }
 		
 		var ii = 0;
 		while (jobs.length > 0) {
@@ -138,61 +117,32 @@ class DemoPagedScreen extends com.pblabs.components.ui.PagedScreen
 			
 			var randId = Std.int(Math.random() * 100000);
 			
-			createBaseSceneEntity()
-				.addSvg(_layerForeground, Resources.list.BUTTON_TEXT_LINE, 
+			//Button
+			var button = createBaseSceneEntity()
+				.addSvg(_layerForeground, Resources.list.BUTTON_ICON_LEFT,
 					[
 						new SvgReplace("$replace", label),
-						// new SvgReplace("fill:url(#linearGradient3800)", "fill:#ffffff"),
-						new SvgReplace("fill:url(#linearGradient3291)", "fill:#ffffff"),
-						
 					], 
 					"c1", false)
-						
-				.addSvg(_layerForeground, Resources.list.BUTTON_TEXT_LINE, 
+				.addSvg(_layerForeground, Resources.list.BUTTON_ICON_LEFT,
 					[
 						new SvgReplace("$replace", label), 
-						// new SvgReplace("fill:#ffffff;fill-opacity:1;stroke:#d9d9d9", "fill:#000000;fill-opacity:1;stroke:#d9d9d9"),
-						new SvgReplace("text-anchor:start;fill:#000000", "text-anchor:start;fill:#ffffff"),
-						// new SvgReplace("linearGradient3794", "linearGradient" + randId),
-						// new SvgReplace("linearGradient3800", "linearGradient" + (randId + 5)),
-						// new SvgReplace("stop3796", "stop" + (randId + 1)),
-						// new SvgReplace("stop3798", "stop" + (randId + 2)),
-						// new SvgReplace("id=\"svg2\"", "id=\"svg2" + (randId + 3) + "\""),
-						// new SvgReplace("defs4", "defs" + (randId + 4)),
 					],
 					"c2", false, true)
-				// .addSvg(_layerForeground, Resources.list.BUTTON_01, [new SvgReplace("$T", label)], "c1", false)
-				// .addSvg(_layerForeground, Resources.list.BUTTON_01_DOWN, [new SvgReplace("$T", label)], "c2", false)
 				.makeTwoStateButton()
 				.setOnClick(cb)
-				.addToComponentParent(vboxButtons)
+				.addToComponentParent(_contextVbox)
 				.invalidateOnOrientationChange()
+				.showOverlay()
 				.initializeEntity("button");
 			
+			//Button icon
+			createBaseSceneEntity()
+				.addSceneComponent(CircleShape, _layerForeground)
+				.initializeEntity("icon")
+				.setEntityAsDisplayChildOf(button, Resources.list.BUTTON_ICON_LEFT, "anchor");
+			
 			ii += 2;
-		}
-		
-		_contextVbox = createVBox(_layerForeground)
-			.addToComponentParent(vboxButtons);
-			
-		createEntity()
-			.addSceneComponent(com.pblabs.components.scene2D.js.CustomHtml, _layerForeground)
-			.initializeEntity("sdfsdf");
-	}
-	
-	override public function enter () :Void
-	{
-		super.enter();
-		for (childButton in _contextVbox.getComponent(Container).children.copy()) {
-			childButton.owner.destroy();
-		}
-		
-		
-		//Create a menu for each context
-		for (context in getManager(PBGameBase).contexts) {
-			if (context == this) continue;
-			
-			
 		}
 	}
 	
@@ -214,13 +164,10 @@ class DemoPagedScreen extends com.pblabs.components.ui.PagedScreen
 		_backButton = createBaseSceneEntity()
 			.addSvg(_layerForeground, Resources.list.BUTTON_BACK, [new SvgReplace("$T", text)], "c1", false)
 			.addSvg(_layerForeground, Resources.list.BUTTON_BACK_DOWN, [new SvgReplace("$T", text)], "c2", false, true)
-			// .addSvg(_layerForeground, Resources.list.BUTTON_BACK, [new SvgReplace("$T", text)], "c1")
-			// .addSvg(_layerForeground, Resources.list.BUTTON_BACK_DOWN, [new SvgReplace("$T", text)], "c2")
 			.makeTwoStateButton()
 			.setOnClick(callback(getManager(PBGameBase).popContext, ScreenTransitions.scrollBack))
 			.initializeEntity("backButton");
 		_backButton.setLocation(topLeft.x + _backButton.getWidth() / 2 + 10, topLeft.y + _backButton.getHeight() / 2 + 8);
-		// _backButton.setLocation(topLeft.x + _backButton.getWidth() / 2 + 100, topLeft.y + _backButton.getHeight() / 2 + 8);
 	}
 	
 	override function setTitle () :Void
@@ -236,11 +183,11 @@ class DemoPagedScreen extends com.pblabs.components.ui.PagedScreen
 			_titlePanel.destroy();
 			_titlePanel = null;
 		}
+		
 		_titlePanel = createBaseSceneEntity()
-			.addSvg(_layerBackground, Resources.list.TITLEBAR, [new SvgReplace("$T", "")], null, false)
 			.addToComponentParent(vboxBackground)
+			.addSvg(_layerBackground, Resources.list.TITLEBAR, [new SvgReplace("$T", "")], null, false)
 			.stretchToWidth()
-			.setXToScreenProportion(0.5)
 			.initializeEntity("titlebar");
 		
 		var titleText = createBaseSceneEntity()
@@ -252,18 +199,5 @@ class DemoPagedScreen extends com.pblabs.components.ui.PagedScreen
 			.addToComponentParent(vboxForeground)
 			.setXToScreenProportion(0.5)
 			.initializeEntity("titletext");
-			
-		// var titletext = createBaseSceneEntity()
-		// 	.addSvg(_layerBackground, Resources.list.TEXT_TITLE, [new SvgReplace("$T", "wetwetwetwet")], null, false)
-		// 	.addToComponentParent(vboxBackground)
-		// 	.initializeEntity("titletext");
-		
-		// var testtext = createBaseSceneEntity()
-		// 	.addSvg(_layerBackground, Resources.list.TEXT_TEST, [new SvgReplace("$T", "sdfgsdfgsdfgsfd")], null, false)
-		// 	.addToComponentParent(vboxBackground)
-		// 	.initializeEntity("titletext");
-			
-		// _titlePanel.setWidth(_layerBackground.scene.sceneView.width);
 	}
-
 }
