41171195b393e81b82adc9e33cb4956ce8c20437
dionjwa <dion@transition9.com>
2012-01-03 18:55:40 -0800

    Use the Container/Component EntityComponents for managing relative locations.

--- src/com/pblabs/components/scene2D/HierarchyManager.hx
+++ src/com/pblabs/components/scene2D/HierarchyManager.hx
@@ -7,7 +7,8 @@ import com.pblabs.engine.core.IEntity;
 import com.pblabs.engine.core.PBManager;
 import com.pblabs.engine.resource.IResourceManager;
 import com.pblabs.engine.resource.ResourceToken;
-import com.pblabs.engine.time.IProcessManager;
+
+import com.pblabs.components.minimalcomp.MCompTools;
 
 import org.transition9.util.Comparators;
 import org.transition9.util.F;
@@ -32,7 +33,6 @@ using org.transition9.util.XmlTools;
   * This manager is an alternative to extending existing components.
   */
 class HierarchyManager extends PBManager
-	,implements com.pblabs.engine.time.ITickedObject
 {
 	/** For 'using' */
 	public static function setEntityAsDisplayChildOf (e :IEntity, parent :IEntity, parentResource :ResourceToken, 
@@ -47,29 +47,14 @@ class HierarchyManager extends PBManager
 	public static var ANCHOR_PREFIX = "anchor";
 	public static var INKSCAPE_LABEL = "inkscape:label";
 	
-	@inject("com.pblabs.engine.resource.IResourceManager")
-	var _rsrc :IResourceManager;
-	
-	/**
-	  * Maps <svg id or DisplayObject.name, map of <anchor name, relative location>>  
-	  */
-	var _links :Array<Link>;
+	@inject
+	public var _rsrc :IResourceManager;
 	var _temp :Vec2;
-	public var priority :Int;
 	
 	public function new ()
 	{
 		super();
-		priority = 0;
-		_links = [];
 		_temp = new Vec2();
-	}
-	
-	public function onTick (dt :Float) :Void
-	{
-		for (link in _links) {
-			updateLink(link);
-		}
 	}
 	
 	public function getAnchors (token :ResourceToken) :Map<String, Vec2>
@@ -78,6 +63,7 @@ class HierarchyManager extends PBManager
 		com.pblabs.engine.debug.Profiler.enter("getSvgResource");
 		com.pblabs.engine.debug.Profiler.exit("getSvgResource");
 		com.pblabs.engine.debug.Profiler.enter("_rsrc.get(");
+		org.transition9.util.Assert.isNotNull(_rsrc, ' _rsrc is null');
 		var svg = _rsrc.get(token);
 		com.pblabs.engine.debug.Profiler.exit("_rsrc.get(");
 		org.transition9.util.Assert.isNotNull(svg, ' svg is null');
@@ -87,32 +73,6 @@ class HierarchyManager extends PBManager
 		return val;
 	}
 	
-	function updateLink (link :Link) :Void
-	{
-		com.pblabs.engine.debug.Profiler.enter("updateLink");
-		org.transition9.util.Assert.isNotNull(link);
-		com.pblabs.engine.debug.Profiler.enter("getOffset");
-		var offset = getOffset(link.parentDisplayType, link.childKey);
-		com.pblabs.engine.debug.Profiler.exit("getOffset");
-		if (offset == null) {
-			org.transition9.util.Log.warn("Not a valid match " + link.parentDisplayType + "." + link.childKey);
-			com.pblabs.engine.debug.Profiler.exit("updateLink");
-			return;
-		}
-		com.pblabs.engine.debug.Profiler.enter("rest");
-		org.transition9.util.Assert.isNotNull(link.parent);
-		// var disp = link.parent.owner.getComponent(com.pblabs.components.scene2D.BaseSceneComponent);
-		// trace('disp=' + disp);
-		// if (disp != null) {trace(disp.scaleX);}
-		var locX = link.parent.x + offset.x * (link.parentDisplay != null ? link.parentDisplay.scaleX : 0);
-		var locY = link.parent.y + offset.y * (link.parentDisplay != null ? link.parentDisplay.scaleY : 0);
-		
-		link.childSpatial.setLocation(locX, locY);
-		// link.child.owner.setLocation(locX, locY);
-		com.pblabs.engine.debug.Profiler.exit("rest");
-		com.pblabs.engine.debug.Profiler.exit("updateLink");
-	}
-	
 	public function setAsChild (parent :IEntity, parentResource :ResourceToken, 
 		childKey :String, child :BaseSceneComponent<Dynamic>) :Void
 	{
@@ -120,67 +80,17 @@ class HierarchyManager extends PBManager
 		org.transition9.util.Assert.isNotNull(childKey);
 		org.transition9.util.Assert.isNotNull(child);
 		org.transition9.util.Assert.isNotNull(parentResource);
-		
-		removeChild(child);
-		
-		var anchors = getAnchors(parentResource); 
-		
-		for (key in anchors.keys()) {
-			if (key.startsWith(childKey)) {
-				childKey = key;
-				break;
-			}
-		}
-		var link = new Link();
-		link.child = child;
-		link.parent = parent.getComponent(SpatialComponent);
-		link.parentDisplay = parent.getComponent(BaseSceneComponent);
-		
-		link.childSpatial = child.owner.getComponent(SpatialComponent);
-		link.parentDisplayType = parentResource;
-		link.childKey = childKey;
-		link.childEntityName = child.owner.name;
-		_links.push(link);
-		updateLink(link);
-		
-		// if (link.parent != null) {
-		// 	var self = this;
-		// 	child.bindSignal(link.parent.signalerLocation, F.ignoreArg(callback(updateLink, link)));
-		// } else {
-		// 	org.transition9.util.Log.warn("Missing SpatialComponent on the parent");
-		// }
-	}
 	
-	override public function startup () :Void
-	{
-		super.startup();
-		context.getManager(IProcessManager).addTickedObject(this);
+		var offset = getOffset(parentResource, childKey);
+		org.transition9.util.Assert.isNotNull(offset, ' offset is null');
+		
+		MCompTools.setRelative(child.owner, parent, offset.x, offset.y);
 	}
 	
 	override public function shutdown () :Void
 	{
-		context.getManager(IProcessManager).removeTickedObject(this);
 		super.shutdown();
 		_rsrc = null;
-		_links = null;
-	}
-	
-	function entityDestroyed (e :IEntity) :Void
-	{
-		for (link in _links.copy()) {
-			if (link.childEntityName == e.name) {
-				_links.remove(link);
-			}
-		}
-	}
-	
-	function removeChild (child :BaseSceneComponent<Dynamic>) :Void
-	{
-		for (link in _links.copy()) {
-			if (link.childEntityName == child.owner.name) {
-				_links.remove(link);
-			}
-		}
 	}
 	
 	function getOffset (parent :ResourceToken, child :String) :Vec2
@@ -198,17 +108,4 @@ class HierarchyManager extends PBManager
 		_temp.y = val.y;
 		return _temp;
 	}
-}
-
-class Link
-{
-	public var child :BaseSceneComponent<Dynamic>;
-	public var childSpatial :SpatialComponent<Dynamic>;
-	public var childEntityName :String;
-	public var parent :SpatialComponent<Dynamic>;
-	public var parentDisplay :BaseSceneComponent<Dynamic>;
-	public var childKey :String;
-	public var parentDisplayType :ResourceToken;
-	
-	public function new () {}
 }
